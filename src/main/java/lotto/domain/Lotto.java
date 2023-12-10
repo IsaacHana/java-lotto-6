daa8ad04d3818f8acb60f4ce284ae1bf8c404b6d
Isaac Han <isaachan233@gmail.com>
2023-12-10 21:58:46 +0900

    feat: 문자열을 매개변수로 받을 수 있도록 생성자 추가 및 toString 정의

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -2,24 +2,42 @@ package lotto.domain;
 
 import lotto.common.Rule;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Lotto {
     private final List<Integer> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    private Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = numbers.stream().sorted().toList();
+    }
+
+    public static Lotto valueOf(List<Integer> numbers) {
+        return new Lotto(numbers);
+    }
+
+    public static Lotto valueOf(String numbers) {
+        try {
+            return valueOf(Arrays.stream(numbers.split(",")).map(Integer::parseInt).toList());
+        } catch (NumberFormatException nfe) {
+            throw new IllegalArgumentException("[ERROR] 유효하지않은 숫자입니다.");
+        }
     }
 
     private void validate(List<Integer> numbers) {
         if (numbers.size() != Rule.MAX_SIZE.getValue()) {
-            throw new IllegalArgumentException("[ERROR 로또는 6자리의 번호여야 합니다.]");
+            throw new IllegalArgumentException("[ERROR] 로또는 중복되지 않은 6자리의 번호여야 합니다.");
         }
 
         if (numbers.stream().distinct().count() != Rule.MAX_SIZE.getValue()) {
-            throw new IllegalArgumentException("[ERROR 로또는 중복되지 않은 6자리의 번호여야 합니다.]");
+            throw new IllegalArgumentException("[ERROR] 로또는 중복되지 않은 6자리의 번호여야 합니다.");
         }
+    }
+
+    @Override
+    public String toString() {
+        return this.numbers.toString();
     }
 
     // TODO: 추가 기능 구현
